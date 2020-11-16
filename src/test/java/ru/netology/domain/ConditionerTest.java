57b3070d2f27089322cb5c52dedefc672581f031
hYxA <zybu@live.ru>
2020-11-16 14:22:57 +0300

    create conditioner and ConditionerTest

---
+++ src/test/java/ru/netology/domain/ConditionerTest.java
@@ -0,0 +1,14 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConditionerTest {
+    @Test
+    public void shouldCreate() {
+        Conditioner conditioner = new Conditioner();
+
+        System.out.println("Hi!");
+    }
+}
