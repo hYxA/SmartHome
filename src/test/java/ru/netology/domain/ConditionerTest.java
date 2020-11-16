82b8d3b13fc16fb4617142cc21a72ae566395a8b
hYxA <zybu@live.ru>
2020-11-16 16:28:12 +0300

    вшыфидув

--- src/test/java/ru/netology/domain/ConditionerTest.java
+++ src/test/java/ru/netology/domain/ConditionerTest.java
@@ -1,5 +1,6 @@
 package ru.netology.domain;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,7 +8,35 @@ import static org.junit.jupiter.api.Assertions.*;
 class ConditionerTest {
     @Test
     public void shouldCreate() {
+        Conditioner conditioner1 = new Conditioner();
+        Conditioner conditioner2 = new Conditioner();
+        Conditioner conditioner3 = new Conditioner();
+
+        conditioner1.currentTemperature = 25;
+
+        System.out.println("Hi!");
+    }
+
+    @Test
+    public void shouldInit() {
         Conditioner conditioner = new Conditioner();
+
+        assertNull(conditioner.name);
+        assertEquals(0, conditioner.maxTemperature);
+        assertEquals(0, conditioner.minTemperature);
+        assertEquals(0, conditioner.currentTemperature);
+        assertFalse(conditioner.on);
+
+        System.out.println("Hi!");
+    }
+
+    @Test
+    @Disabled
+    public void shouldThrowNpe() {
+        Conditioner conditioner = new Conditioner();
+
+        assertEquals(0, conditioner.name.length());
+
 
         System.out.println("Hi!");
     }
