035083a7cd4e0d1187edcc6eb36df92c91182b7e
hYxA <zybu@live.ru>
2020-11-17 12:31:00 +0300

    add Getters and Setters

--- src/main/java/ru/netology/domain/ConditionerAdvanced.java
+++ src/main/java/ru/netology/domain/ConditionerAdvanced.java
@@ -7,4 +7,43 @@ public class ConditionerAdvanced {
     private int currentTemperature;
     private boolean on;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMaxTemperature() {
+        return maxTemperature;
+    }
+
+    public void setMaxTemperature(int maxTemperature) {
+        this.maxTemperature = maxTemperature;
+    }
+
+    public int getMinTemperature() {
+        return minTemperature;
+    }
+
+    public void setMinTemperature(int minTemperature) {
+        this.minTemperature = minTemperature;
+    }
+
+    public int getCurrentTemperature() {
+        return currentTemperature;
+    }
+
+    public void setCurrentTemperature(int currentTemperature) {
+        this.currentTemperature = currentTemperature;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
 }
