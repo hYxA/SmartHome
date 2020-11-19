b8434b06972371e5a8511ce703e50cf4cb3ccb65
hYxA <zybu@live.ru>
2020-11-19 11:44:09 +0300

    added getters and setters

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -4,4 +4,23 @@ public class Radio {
 
     private int number; //  Номер текущей (прослушиваемой) радиостанции
     private int volume; //  Громкость звука
+
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
 }
