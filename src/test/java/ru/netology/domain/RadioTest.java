35689a54284adac63a68b1c6c1205b48e4622833
hYxA <zybu@live.ru>
2020-11-19 16:00:43 +0300

    create RadioTest.java

---
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -0,0 +1,56 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+
+    @Test
+    void getMaxVolume() {
+    }
+
+    @Test
+    void setMaxVolume() {
+    }
+
+    @Test
+    void getMinVolume() {
+    }
+
+    @Test
+    void setMinVolume() {
+    }
+
+    @Test
+    void getCurrentNumber() {
+    }
+
+    @Test
+    void setCurrentNumber() {
+    }
+
+    @Test
+    void getMaxNumber() {
+    }
+
+    @Test
+    void setMaxNumber() {
+    }
+
+    @Test
+    void getMinNumber() {
+    }
+
+    @Test
+    void setMinNumber() {
+    }
+
+    @Test
+    void getVolume() {
+    }
+
+    @Test
+    void setVolume() {
+    }
+}
